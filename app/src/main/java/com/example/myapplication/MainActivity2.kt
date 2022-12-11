51214f2a2b18824e46685bea0fe5026f61946108
nikita <nini.vardidze.1@btu.edu.ge>
2022-12-11 14:56:27 +0400

    Pass data between activities

---
+++ app/src/main/java/com/example/myapplication/MainActivity2.kt
@@ -0,0 +1,18 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val message = intent.getStringExtra("EXTRA_MESSAGE")
+
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
+    }
+}
